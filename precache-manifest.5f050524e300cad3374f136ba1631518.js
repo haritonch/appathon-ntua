16a11701f33f0b687ab10e297f1e7a421c01be4e
haritonch <haritonch@yahoo.com>
2020-08-07 22:19:06 +0300

    Updates

---
+++ precache-manifest.5f050524e300cad3374f136ba1631518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e691a1da81e28548299db7a32a2133e5",
+    "url": "/appathon-ntua/index.html"
+  },
+  {
+    "revision": "ab0a4b4badc6493a09bd",
+    "url": "/appathon-ntua/static/css/main.6bc24ede.chunk.css"
+  },
+  {
+    "revision": "0c3bd9e8bc34c1195a9c",
+    "url": "/appathon-ntua/static/js/2.b62a0d69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appathon-ntua/static/js/2.b62a0d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0a4b4badc6493a09bd",
+    "url": "/appathon-ntua/static/js/main.b4abd15c.chunk.js"
+  },
+  {
+    "revision": "5d2365be1b00f2f13449",
+    "url": "/appathon-ntua/static/js/runtime-main.29350820.js"
+  }
+]);
