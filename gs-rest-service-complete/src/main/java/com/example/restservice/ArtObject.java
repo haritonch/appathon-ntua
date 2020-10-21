075a47cbc9101353572150137ce11cca518b5f8e
haritonch <haritonch@yahoo.com>
2020-10-21 21:15:18 +0300

    change initia object in java service

--- gs-rest-service-complete/src/main/java/com/example/restservice/ArtObject.java
+++ gs-rest-service-complete/src/main/java/com/example/restservice/ArtObject.java
@@ -8,41 +8,41 @@ public class ArtObject {
 	private final String artistDisplayName;
 	private final String objectURL;
 	private final String country;
-	
+
 	public ArtObject() {
-		this.objectID = 436532;
-		this.primaryImageSmall = "https://images.metmuseum.org/CRDImages/ep/web-large/DT1502_cropped2.jpg";
-		this.title = "Self-Portrait with a Straw Hat (obverse: The Potato Peeler)";
+		this.objectID = 437549;
+		this.primaryImageSmall = "https://images.metmuseum.org/CRDImages/ep/web-large/DP145911.jpg";
+		this.title = "Wheat Fields";
 		this.department = "European Paintings";
-		this.artistDisplayName = "Vincent van Gogh";
-		this.objectURL = "https://www.metmuseum.org/art/collection/search/436532";
+		this.artistDisplayName = "Jacob van Ruisdael";
+		this.objectURL = "https://www.metmuseum.org/art/collection/search/437549";
 		this.country = "";
 	}
-	
+
 	public long getObjectID() {
 		return this.objectID;
 	}
-	
+
 	public String getPrimaryImageSmall() {
 		return this.primaryImageSmall;
 	}
-	
+
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	public String getDepartment() {
 		return this.department;
 	}
-	
+
 	public String getArtistDisplayName() {
 		return this.artistDisplayName;
 	}
-	
+
 	public String getObjectURL() {
 		return this.objectURL;
 	}
-	
+
 	public String getCountry() {
 		return this.country;
 	}
